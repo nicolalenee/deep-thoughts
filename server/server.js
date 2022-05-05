062f97f9661bbea1c54273424a228c8c051c879d
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-05-05 18:55:41 -0400

    commit unsaved changes

--- server/server.js
+++ server/server.js
@@ -39,6 +39,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+// check if node environment is in production
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
