b53b26cda77445b43dae375c23f594bf9fb3eabf
cola-mars <86696492+cola-mars@users.noreply.github.com>
2023-01-26 21:21:21 -0500

    fix db conn

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-console.log(process.env.MONGODB_URI);
+
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/deep-thoughts",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   }
 );
 
