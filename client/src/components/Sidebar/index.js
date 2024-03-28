92f0ec4be26c894b09cf395763159d8533f4ac02
cola-mars <86696492+cola-mars@users.noreply.github.com>
2024-03-28 14:45:27 -0400

    add Edit Profile btn to sidebar

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -8,10 +8,11 @@ const Sidebar = ({ username }) => {
           <img className="profile-picture" src="/kirby.png" alt="profile" />
           <h2 className="username">{username}</h2>
           <div className="card-body">
-            <p className="bio">
+            <p className="bio-text">
               Hey there! My name's Nicola. I love to hike, dance, and read.
               Currently, I'm craving some Mexican.
             </p>
+            <btn className="btn pill form-input">Edit Profile</btn>
           </div>
         </div>
       </div>
